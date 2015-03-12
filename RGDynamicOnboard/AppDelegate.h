85847e030f376cce1f610d138a14002ce09d59ac
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-03-12 19:24:13 +0100

    First commit

---
+++ RGDynamicOnboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RGDynamicOnboard
+//
+//  Created by Ramon Gilabert Llop on 3/12/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RGDynamicOnboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RGDynamicOnboard
+//
+//  Created by Ramon Gilabert Llop on 3/12/15.
+//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
