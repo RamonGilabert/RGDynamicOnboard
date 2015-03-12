61f3f2a6cf12753a8078f9a12bbd181a2681f81f
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-03-12 19:40:31 +0100

    Refactoring

--- RGDynamicOnboard/RGDynamicOnboard.h
+++ RGDynamicOnboard/RGDynamicOnboard.h
@@ -1,11 +1,3 @@
-//
-//  RGDynamicOnboard.h
-//  RGDynamicOnboard
-//
-//  Created by Ramon Gilabert Llop on 3/12/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface RGDynamicOnboard : UIView
--- RGDynamicOnboard/RGDynamicOnboard.m
+++ RGDynamicOnboard/RGDynamicOnboard.m
@@ -1,11 +1,3 @@
-//
-//  RGDynamicOnboard.m
-//  RGDynamicOnboard
-//
-//  Created by Ramon Gilabert Llop on 3/12/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 #import "RGDynamicOnboard.h"
 
 @implementation RGDynamicOnboard
