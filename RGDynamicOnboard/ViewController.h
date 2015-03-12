393d08121321b47f3e4fa0f3b3c71c5639af0cea
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-03-12 19:34:59 +0100

    Added some files, refactored some others

--- RGDynamicOnboard/ViewController.h
+++ RGDynamicOnboard/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  RGDynamicOnboard
-//
-//  Created by Ramon Gilabert Llop on 3/12/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
--- RGDynamicOnboard/ViewController.m
+++ RGDynamicOnboard/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  RGDynamicOnboard
-//
-//  Created by Ramon Gilabert Llop on 3/12/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -14,14 +6,9 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
